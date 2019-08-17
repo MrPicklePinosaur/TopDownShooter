50c45f98bcdeeb930a83e306ceb1021354029e27
Daniel Liu <mr.picklepinosaur@gmail.com>
2019-08-17 19:28:14 -0400

    init commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float camera_speed;
+    public GameObject target;
+
+    private Vector3 target_pos;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        target_pos = new Vector3(target.transform.position.x,target.transform.position.y,transform.position.z);
+        transform.position = Vector3.Lerp(transform.position,target_pos,camera_speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private static float INPUT_THRESHHOLD = 0.5f;
+
+    public float move_speed;
+
+    private Animator anim;
+    private bool isMoving;
+    private Vector2 prevMove;
+
+    // Start is called before the first frame update
+    void Start() {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        isMoving = false;
+
+        //Check to see if there is actually input
+        float input_x = Input.GetAxisRaw("Horizontal");
+        float input_y = Input.GetAxisRaw("Vertical");
+        if (Math.Abs(input_x) > INPUT_THRESHHOLD || Math.Abs(input_y) > INPUT_THRESHHOLD) {
+            transform.Translate(new Vector3(input_x * move_speed * Time.deltaTime, input_y * move_speed * Time.deltaTime, 0f));
+            prevMove = new Vector2(input_x,input_y);
+            isMoving = true;
+        }
+
+        //Update animator
+        anim.SetFloat("MoveX", input_x);
+        anim.SetFloat("MoveY", input_y);
+        anim.SetFloat("PrevMoveX", prevMove.x);
+        anim.SetFloat("PrevMoveY", prevMove.y);
+        anim.SetBool("isMoving", isMoving);
+
+
+    }
+}
