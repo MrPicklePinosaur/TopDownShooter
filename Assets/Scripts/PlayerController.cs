05e45f562e396bd0c4e38b795ed95e8c1762a573
Daniel Liu <mr.picklepinosaur@gmail.com>
2019-08-30 19:29:53 -0400

    started work on enemy and reworked player animator to use blend tree

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour {
     private static float INPUT_THRESHHOLD = 0.5f;
     private static bool isAlive = false;
 
+    [Header("Basic Vars")]
     [Range(0f,10f)]
     public float move_speed;
 
@@ -16,20 +17,13 @@ public class PlayerController : MonoBehaviour {
     private bool isMoving;
     private Vector2 prevMove;
 
+
     // Start is called before the first frame update
     void Start() {
         anim = GetComponent<Animator>();
         body = GetComponent<Rigidbody2D>();
 
         DontDestroyOnLoad(transform.gameObject);
-        /*
-        if (!isAlive) {
-            isAlive = true;
-            DontDestroyOnLoad(transform.gameObject);
-        } else {
-            Destroy(gameObject);
-        }
-        */
     }
 
     // Update is called once per frame
@@ -42,7 +36,6 @@ public class PlayerController : MonoBehaviour {
         float input_x = Input.GetAxisRaw("Horizontal");
         float input_y = Input.GetAxisRaw("Vertical");
         if (Math.Abs(input_x) > INPUT_THRESHHOLD || Math.Abs(input_y) > INPUT_THRESHHOLD) {
-            //transform.Translate(new Vector3(input_x * move_speed * Time.deltaTime, input_y * move_speed * Time.deltaTime, 0f));
             body.velocity = new Vector2(input_x * move_speed, input_y * move_speed);
             prevMove = new Vector2(input_x,input_y);
             isMoving = true;
@@ -51,6 +44,9 @@ public class PlayerController : MonoBehaviour {
         //Update animator
         anim.SetFloat("MoveX", input_x);
         anim.SetFloat("MoveY", input_y);
+        anim.SetBool("isMoving", isMoving);
+        anim.SetFloat("PrevX", prevMove.x);
+        anim.SetFloat("PrevY", prevMove.y);
 
     }
 }
---
+++ Assets/Scripts/SkeletController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/*
+skeleton ai consists of a wandering stage and a target stage once
+the player is within detection range
+*/
+
+public class SkeletController : MonoBehaviour {
+
+    public static System.Random rand = new System.Random();
+
+    [Header("Basic Vars")]
+    [Range(0f, 10f)]
+    public float move_speed;
+
+    private Animator anim;
+    private Rigidbody2D body;
+    private bool isMoving;
+    private Vector2 prevMove;
+
+    [Header("AI Vars")]
+    private float moveDirect; //an angle measurement, in radians
+
+    void Start() {
+        anim = GetComponent<Animator>();
+        body = GetComponent<Rigidbody2D>();
+
+        //decide which direction to move in
+        moveDirect = (float)(SkeletController.rand.Next(0, 360) * (Math.PI / 180));
+    } 
+
+    void Update() {
+        isMoving = false;
+        body.velocity = new Vector2(0, 0);
+
+        float input_x = (float)Math.Cos(moveDirect);
+        float input_y = (float)Math.Sin(moveDirect);
+
+        //Check to see if there is actually input
+        if (move_speed > 0) {
+        
+            body.velocity = new Vector2(input_x * move_speed, input_y * move_speed);
+            prevMove = new Vector2(input_x, input_y);
+            isMoving = true;
+        }
+
+        //Update animator
+        anim.SetFloat("MoveX", input_x);
+        anim.SetFloat("MoveY", input_y);
+    }
+    
+}
